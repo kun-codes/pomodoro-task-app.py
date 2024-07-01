361e8b1a701f46a5d5e501a12139305532cd26e1
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-01 12:39:46 +0530

    Add main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QWidget, QApplication
 
-from src.views.tasks_view import TasksView
+from src.views.tasks_view import TaskListView
+# from src.ui.converted.task_card_ui import Ui_Form as TaskCardUi
 
-app = QApplication(sys.argv)
-window = TasksView()
-window.show()
-app.exec()
+from qfluentwidgets import FluentIcon, FluentWindow, setTheme, Theme
+
+class MainWindow(FluentWindow):
+    def __init__(self):
+        super().__init__()
+        self.task_interface = TaskListView()
+
+
+        self.initNavigation()
+        self.initWindow()
+
+    def initNavigation(self):
+
+        # Add sub interface
+        self.addSubInterface(self.task_interface, FluentIcon.ADD, 'Tasks')
+
+    def initWindow(self):
+        self.resize(1000, 800)
+        self.setWindowTitle('Pomodoro Task List App')
+
+if __name__ == '__main__':
+    setTheme(Theme.DARK)
+
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    app.exec()
