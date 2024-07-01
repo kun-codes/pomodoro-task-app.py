3529f747facf1039d2b2df85b09aad13a82dfda0
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-01 16:22:55 +0530

    Add functionality of adding task to add task button in task view

--- src/views/tasks_view.py
+++ src/views/tasks_view.py
@@ -1,8 +1,9 @@
 from PySide6.QtWidgets import QWidget
 
-from qfluentwidgets import FluentIcon
+from qfluentwidgets import FluentIcon, MessageBoxBase
 
 from src.ui_py.ui_tasks_list_view import Ui_TaskView
+from src.prefabs.addTaskDialog import AddTaskDialog
 
 
 class TaskListView(QWidget, Ui_TaskView):
@@ -12,3 +13,9 @@ class TaskListView(QWidget, Ui_TaskView):
 
         # set icons of buttons
         self.addTaskButton.setIcon(FluentIcon.ADD)
+        self.addTaskButton.clicked.connect(self.addTask)
+
+    def addTask(self):
+        dialog = AddTaskDialog(self)
+        dialog.show()
+
