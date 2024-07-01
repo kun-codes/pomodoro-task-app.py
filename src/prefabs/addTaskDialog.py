abd22402a70c91c454bb9552afd5c7880d455b4b
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-01 16:22:23 +0530

    Add a task dialog prefab

---
+++ src/prefabs/addTaskDialog.py
@@ -0,0 +1,25 @@
+import sys
+
+from qfluentwidgets import MessageBoxBase, SubtitleLabel, LineEdit
+
+
+class AddTaskDialog(MessageBoxBase):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.titleLabel = SubtitleLabel("Add Task", self)
+        self.taskEdit = LineEdit(self)
+
+        self.taskEdit.setPlaceholderText("Enter task name")
+
+        self.viewLayout.addWidget(self.titleLabel)
+        self.viewLayout.addWidget(self.taskEdit)
+
+        self.yesButton.setText("Add Task")
+        self.cancelButton.setText("Cancel")
+
+        self.widget.setMinimumWidth(max(350, int(parent.width() * 0.3)))
+        self.yesButton.setDisabled(True)
+        self.taskEdit.textChanged.connect(self.onTaskTextChanged)
+
+    def onTaskTextChanged(self):
+        self.yesButton.setDisabled(self.taskEdit.text().strip() == "")
