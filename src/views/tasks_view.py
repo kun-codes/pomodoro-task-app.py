320707b151e575bae1fbcc26849b113cc885eaa7
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-01 18:51:34 +0530

    Change tasks_view.py to implement labels and cards using code. Labels are now DragWidget(s)

--- src/views/tasks_view.py
+++ src/views/tasks_view.py
@@ -1,15 +1,36 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QApplication, QSizePolicy
 
-from qfluentwidgets import FluentIcon, MessageBoxBase
+from qfluentwidgets import FluentIcon, MessageBoxBase, TitleLabel
 
 from src.ui_py.ui_tasks_list_view import Ui_TaskView
 from src.prefabs.addTaskDialog import AddTaskDialog
+from src.models.drag_and_drop import DragWidget
 
 
 class TaskListView(QWidget, Ui_TaskView):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        label_size_policy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+
+        self.todoTasksLabel = TitleLabel("Todo Tasks")
+        self.todoTasksLabel.setSizePolicy(label_size_policy)
+        self.scrollAreaWidgetContents.layout().addWidget(self.todoTasksLabel)
+
+        # Add the todoTasksCard to the taskListView
+        self.todoTasksCard = DragWidget()
+        self.scrollAreaWidgetContents.layout().addWidget(self.todoTasksCard)
+
+        self.completedTasksLabel = TitleLabel("Completed Tasks")
+        self.completedTasksLabel.setSizePolicy(label_size_policy)
+        self.scrollAreaWidgetContents.layout().addWidget(self.completedTasksLabel)
+
+        # Add the completedTasksCard to the taskListView
+        self.completedTasksCard = DragWidget()
+        self.scrollAreaWidgetContents.layout().addWidget(self.completedTasksCard)
+
 
         # set icons of buttons
         self.addTaskButton.setIcon(FluentIcon.ADD)
@@ -19,3 +40,10 @@ class TaskListView(QWidget, Ui_TaskView):
         dialog = AddTaskDialog(self)
         dialog.show()
 
+
+if __name__ == "__main__":
+
+    app = QApplication()
+    w = TaskListView()
+    w.show()
+    app.exec()
