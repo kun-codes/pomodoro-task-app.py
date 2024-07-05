8a53eb7ca0a45aba40a9bb76495259b68abac6d3
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-05 14:22:35 +0530

    Remove the background card from pomodoro view

--- src/ui_py/ui_pomodoro_view.py
+++ src/ui_py/ui_pomodoro_view.py
@@ -15,25 +15,21 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLayout,
-    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLayout, QSizePolicy,
+    QSpacerItem, QVBoxLayout, QWidget)
 
-from qfluentwidgets import (CardWidget, PillToolButton, ProgressBar, ProgressRing,
-    TitleLabel, ToggleToolButton, ToolButton)
+from qfluentwidgets import (PillToolButton, ProgressBar, ProgressRing, TitleLabel,
+    ToggleToolButton, ToolButton)
 
 class Ui_PomodoroView(object):
     def setupUi(self, PomodoroView):
         if not PomodoroView.objectName():
             PomodoroView.setObjectName(u"PomodoroView")
         PomodoroView.resize(486, 489)
-        self.gridLayout = QGridLayout(PomodoroView)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.CardWidget = CardWidget(PomodoroView)
-        self.CardWidget.setObjectName(u"CardWidget")
-        self.verticalLayout = QVBoxLayout(self.CardWidget)
-        self.verticalLayout.setSpacing(21)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.pomodoroLabel = TitleLabel(self.CardWidget)
+        self.verticalLayout_2 = QVBoxLayout(PomodoroView)
+        self.verticalLayout_2.setSpacing(21)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.pomodoroLabel = TitleLabel(PomodoroView)
         self.pomodoroLabel.setObjectName(u"pomodoroLabel")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -41,13 +37,13 @@ class Ui_PomodoroView(object):
         sizePolicy.setHeightForWidth(self.pomodoroLabel.sizePolicy().hasHeightForWidth())
         self.pomodoroLabel.setSizePolicy(sizePolicy)
 
-        self.verticalLayout.addWidget(self.pomodoroLabel)
+        self.verticalLayout_2.addWidget(self.pomodoroLabel)
 
         self.topVerticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout.addItem(self.topVerticalSpacer)
+        self.verticalLayout_2.addItem(self.topVerticalSpacer)
 
-        self.ProgressRing = ProgressRing(self.CardWidget)
+        self.ProgressRing = ProgressRing(PomodoroView)
         self.ProgressRing.setObjectName(u"ProgressRing")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
@@ -58,7 +54,7 @@ class Ui_PomodoroView(object):
         self.ProgressRing.setMaximumSize(QSize(220, 220))
         self.ProgressRing.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.verticalLayout.addWidget(self.ProgressRing, 0, Qt.AlignmentFlag.AlignHCenter)
+        self.verticalLayout_2.addWidget(self.ProgressRing, 0, Qt.AlignmentFlag.AlignHCenter)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(21)
@@ -68,7 +64,7 @@ class Ui_PomodoroView(object):
 
         self.horizontalLayout.addItem(self.leftHorizontalSpacer)
 
-        self.stopButton = PillToolButton(self.CardWidget)
+        self.stopButton = PillToolButton(PomodoroView)
         self.stopButton.setObjectName(u"stopButton")
         self.stopButton.setMinimumSize(QSize(68, 68))
         self.stopButton.setIconSize(QSize(21, 21))
@@ -76,7 +72,7 @@ class Ui_PomodoroView(object):
 
         self.horizontalLayout.addWidget(self.stopButton)
 
-        self.pauseResumeButton = PillToolButton(self.CardWidget)
+        self.pauseResumeButton = PillToolButton(PomodoroView)
         self.pauseResumeButton.setObjectName(u"pauseResumeButton")
         self.pauseResumeButton.setMinimumSize(QSize(68, 68))
         self.pauseResumeButton.setIconSize(QSize(21, 21))
@@ -84,7 +80,7 @@ class Ui_PomodoroView(object):
 
         self.horizontalLayout.addWidget(self.pauseResumeButton)
 
-        self.skipButton = PillToolButton(self.CardWidget)
+        self.skipButton = PillToolButton(PomodoroView)
         self.skipButton.setObjectName(u"skipButton")
         self.skipButton.setMinimumSize(QSize(68, 68))
         self.skipButton.setIconSize(QSize(21, 21))
@@ -97,14 +93,11 @@ class Ui_PomodoroView(object):
         self.horizontalLayout.addItem(self.rightHorizontalSpacer)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
 
         self.bottomVerticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout.addItem(self.bottomVerticalSpacer)
-
-
-        self.gridLayout.addWidget(self.CardWidget, 0, 0, 1, 1)
+        self.verticalLayout_2.addItem(self.bottomVerticalSpacer)
 
 
         self.retranslateUi(PomodoroView)
