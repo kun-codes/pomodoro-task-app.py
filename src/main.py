87e208e2a1aa11f5380ae40675dcc047d4710686
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-07 14:54:15 +0530

    Add settings interface to main.py

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ from PySide6.QtWidgets import QWidget, QApplication
 
 from views.tasks_view import TaskListView
 from views.pomodoro_view import PomodoroView
+from views.settings_view import SettingsView
 from models.drag_and_drop import DragWidget, DragItem
 # from src.ui.converted.task_card_ui import Ui_Form as TaskCardUi
 
-from qfluentwidgets import FluentIcon, FluentWindow, setTheme, Theme
+from qfluentwidgets import FluentIcon, FluentWindow, setTheme, Theme, NavigationItemPosition
 
 
 class MainWindow(FluentWindow):
@@ -19,6 +20,9 @@ class MainWindow(FluentWindow):
         self.pomodoro_interface = PomodoroView()
         self.pomodoro_interface.setObjectName('pomodoro_interface')
 
+        self.settings_interface = SettingsView()
+        self.settings_interface.setObjectName('settings_interface')
+
         self.initNavigation()
         self.initWindow()
         self.populateTasks()
@@ -27,6 +31,10 @@ class MainWindow(FluentWindow):
         # Add sub interface
         self.addSubInterface(self.task_interface, FluentIcon.ADD, 'Tasks')
         self.addSubInterface(self.pomodoro_interface, FluentIcon.STOP_WATCH, 'Pomodoro')
+
+        # Add sub interface at bottom
+        self.addSubInterface(
+            self.settings_interface, FluentIcon.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
 
     def initWindow(self):
         self.resize(1000, 800)
@@ -46,7 +54,7 @@ class MainWindow(FluentWindow):
 
 
 if __name__ == '__main__':
-    # setTheme(Theme.DARK)
+    setTheme(Theme.DARK)
 
     app = QApplication(sys.argv)
     w = MainWindow()
