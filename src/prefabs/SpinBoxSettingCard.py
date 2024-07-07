b2317dcc3c485b8c4daafc0fd9a544d35852dd39
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-07 17:03:45 +0530

    Add spin box setting card prefab

---
+++ src/prefabs/SpinBoxSettingCard.py
@@ -0,0 +1,64 @@
+from typing import Union
+
+from PySide6.QtCore import Qt
+from PySide6.QtCore import Signal
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QLabel
+from qfluentwidgets import SettingCard, SpinBox, FluentIconBase, qconfig
+
+
+class SpinBoxSettingCard(SettingCard):
+    """ Setting card with a SpinBox """
+
+    valueChanged = Signal(int)
+
+    def __init__(self, configItem, icon: Union[str, QIcon, FluentIconBase], title, content=None, parent=None):
+        """
+        Parameters
+        ----------
+        configItem: RangeConfigItem
+            configuration item operated by the card
+
+        icon: str | QIcon | FluentIconBase
+            the icon to be drawn
+
+        title: str
+            the title of card
+
+        content: str
+            the content of card
+
+        parent: QWidget
+            parent widget
+        """
+        super().__init__(icon, title, content, parent)
+        self.configItem = configItem
+        self.spinBox = SpinBox(self)
+        self.valueLabel = QLabel(self)
+        # self.slider.setMinimumWidth(268)
+
+        self.spinBox.setSingleStep(1)
+        self.spinBox.setRange(*configItem.range)
+        self.spinBox.setValue(configItem.value)
+        # self.valueLabel.setNum(configItem.value)
+
+        self.hBoxLayout.addStretch(1)
+        # self.hBoxLayout.addWidget(self.valueLabel, 0, Qt.AlignRight)
+        self.hBoxLayout.addSpacing(6)
+        self.hBoxLayout.addWidget(self.spinBox, 0, Qt.AlignRight)
+        self.hBoxLayout.addSpacing(16)
+
+        self.valueLabel.setObjectName('valueLabel')
+        configItem.valueChanged.connect(self.setValue)
+        self.spinBox.valueChanged.connect(self.__onValueChanged)
+
+    def __onValueChanged(self, value: int):
+        """ spim box value changed slot """
+        self.setValue(value)
+        self.valueChanged.emit(value)
+
+    def setValue(self, value):
+        qconfig.set(self.configItem, value)
+        self.valueLabel.setNum(value)
+        self.valueLabel.adjustSize()
+        self.spinBox.setValue(value)
