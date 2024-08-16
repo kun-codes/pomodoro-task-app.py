da357f6585e8d89ad5bcf0cf8c1732dce7573448
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-08-16 19:28:57 +0530

    Refactor: Change URL lists to sets
    - changed to ensure uniqueness and faster lookup times when in future website blocking is implemented using mitmproxy

--- pomodoro-task-app/models/website_blocker_model.py
+++ pomodoro-task-app/models/website_blocker_model.py
@@ -17,10 +17,10 @@ class WebsiteBlockerModel(QObject):
     def __init__(self):
         super().__init__()
 
-        self.blocklist_urls = []
-        self.blocklist_exception_urls = []
-        self.allowlist_urls = []
-        self.allowlist_exception_urls = []
+        self.blocklist_urls = set()
+        self.blocklist_exception_urls = set()
+        self.allowlist_urls = set()
+        self.allowlist_exception_urls = set()
 
         self.website_filter_type = None
 
@@ -31,18 +31,19 @@ class WebsiteBlockerModel(QObject):
             current_workspace_id = WorkspaceLookup.get_current_workspace_id()
 
             if target_list == None:
-                self.blocklist_urls = [url.url for url in session.query(BlocklistURL).filter(BlocklistURL.workspace_id == current_workspace_id).all()]
-                self.blocklist_exception_urls = [url.url for url in session.query(BlocklistExceptionURL).filter(BlocklistExceptionURL.workspace_id == current_workspace_id).all()]
-                self.allowlist_urls = [url.url for url in session.query(AllowlistURL).filter(AllowlistURL.workspace_id == current_workspace_id).all()]
-                self.allowlist_exception_urls = [url.url for url in session.query(AllowlistExceptionURL).filter(AllowlistExceptionURL.workspace_id == current_workspace_id).all()]
+                self.blocklist_urls = {url.url for url in session.query(BlocklistURL).filter(BlocklistURL.workspace_id == current_workspace_id).all()}
+                self.blocklist_exception_urls = {url.url for url in session.query(BlocklistExceptionURL).filter(BlocklistExceptionURL.workspace_id == current_workspace_id).all()}
+                self.allowlist_urls = {url.url for url in session.query(AllowlistURL).filter(AllowlistURL.workspace_id == current_workspace_id).all()}
+                self.allowlist_exception_urls = {url.url for url in session.query(AllowlistExceptionURL).filter(AllowlistExceptionURL.workspace_id == current_workspace_id).all()}
             elif target_list == URLListType.BLOCKLIST:
-                self.blocklist_urls = [url.url for url in session.query(BlocklistURL).filter(BlocklistURL.workspace_id == current_workspace_id).all()]
+                self.blocklist_urls = {url.url for url in session.query(BlocklistURL).filter(BlocklistURL.workspace_id == current_workspace_id).all()}
+                logger.debug(f"Inside if condition of load_data() for BLOCKLIST")
             elif target_list == URLListType.BLOCKLIST_EXCEPTION:
-                self.blocklist_exception_urls = [url.url for url in session.query(BlocklistExceptionURL).filter(BlocklistExceptionURL.workspace_id == current_workspace_id).all()]
+                self.blocklist_exception_urls = {url.url for url in session.query(BlocklistExceptionURL).filter(BlocklistExceptionURL.workspace_id == current_workspace_id).all()}
             elif target_list == URLListType.ALLOWLIST:
-                self.allowlist_urls = [url.url for url in session.query(AllowlistURL).filter(AllowlistURL.workspace_id == current_workspace_id).all()]
+                self.allowlist_urls = {url.url for url in session.query(AllowlistURL).filter(AllowlistURL.workspace_id == current_workspace_id).all()}
             elif target_list == URLListType.ALLOWLIST_EXCEPTION:
-                self.allowlist_exception_urls = [url.url for url in session.query(AllowlistExceptionURL).filter(AllowlistExceptionURL.workspace_id == current_workspace_id).all()]
+                self.allowlist_exception_urls = {url.url for url in session.query(AllowlistExceptionURL).filter(AllowlistExceptionURL.workspace_id == current_workspace_id).all()}
 
     def set_website_filter_type(self, website_filter_type: WebsiteFilterType):
         with get_session() as session:
@@ -54,37 +55,37 @@ class WebsiteBlockerModel(QObject):
     def get_website_filter_type(self):
         return self.website_filter_type
 
-    def update_target_list_urls(self, target_list: URLListType, target_list_urls: list):  # todo: find out how to pass the urls as a list
-        valid_urls = []
+    def update_target_list_urls(self, target_list: URLListType, target_list_urls: set):
+        valid_urls = set()
         for line_number, url in enumerate(target_list_urls):
             result = self.validate_url(url)
             if not result:
+                # todo: show error message in UI
                 logger.error(f"Invalid URL at line {line_number + 1}: {url}")
             else:
-                valid_urls.append(url)
-
-        valid_urls_set = set(valid_urls)  # remove duplicates
+                valid_urls.add(url)
 
         with get_session() as session:
 
-            current_urls_set = set()
+            current_urls = set()
             target_class = None
 
             if target_list == URLListType.BLOCKLIST:
-                current_urls_set = set(self.blocklist_urls)
+                current_urls = self.blocklist_urls
                 target_class = BlocklistURL
             elif target_list == URLListType.BLOCKLIST_EXCEPTION:
-                current_urls_set = set(self.blocklist_exception_urls)
+                current_urls = self.blocklist_exception_urls
                 target_class = BlocklistExceptionURL
             elif target_list == URLListType.ALLOWLIST:
-                current_urls_set = set(self.allowlist_urls)
+                current_urls = self.allowlist_urls
                 target_class = AllowlistURL
             elif target_list == URLListType.ALLOWLIST_EXCEPTION:
-                current_urls_set = set(self.allowlist_exception_urls)
+                current_urls = self.allowlist_exception_urls
                 target_class = AllowlistExceptionURL
 
-            urls_to_add = valid_urls_set - current_urls_set  # new url = url not in old set but in new set
-            urls_to_remove = current_urls_set - valid_urls_set # removed url = url not in new set but in old set
+
+            urls_to_add = valid_urls - current_urls  # new url = url not in old set but in new set
+            urls_to_remove = current_urls - valid_urls # removed url = url not in new set but in old set
 
             if urls_to_remove:
                 self.remove_urls(session, urls_to_remove, target_class)
@@ -126,12 +127,12 @@ class WebsiteBlockerModel(QObject):
             return (result, None)
 
     # helper function for update_target_list_urls()
-    def add_urls(self, session, urls, target_class):
+    def add_urls(self, session, urls: set, target_class):
         for url in urls:
             session.add(target_class(workspace_id=WorkspaceLookup.get_current_workspace_id(), url=url))
 
     # helper function for update_target_list_urls()
-    def remove_urls(self, session, urls, target_class):
+    def remove_urls(self, session, urls: set, target_class):
         session.query(target_class).filter(target_class.url.in_(urls)).delete(synchronize_session=False)
 
     def get_urls(self, target_list: URLListType):
