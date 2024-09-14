68c29350261b3aed862a88d808182cac09ed5ed8
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-09-14 13:02:10 +0530

    ui(tasks list view): use SingleDirectionScrollArea instead of QScrollArea

--- pomodoro-task-app/ui_py/ui_tasks_list_view.py
+++ pomodoro-task-app/ui_py/ui_tasks_list_view.py
@@ -8,31 +8,35 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
-from PySide6.QtWidgets import (QGridLayout, QHBoxLayout, QScrollArea,
-                               QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
 
-from qfluentwidgets import ToolButton
-
+from qfluentwidgets import (SingleDirectionScrollArea, ToolButton)
 
 class Ui_TaskView(object):
     def setupUi(self, TaskView):
         if not TaskView.objectName():
             TaskView.setObjectName(u"TaskView")
         TaskView.resize(800, 600)
-        self.gridLayout = QGridLayout(TaskView)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.gridLayout.setContentsMargins(10, 10, 10, -1)
-        self.scrollArea = QScrollArea(TaskView)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setStyleSheet(u"QScrollArea{background: transparent; border: none}")
-        self.scrollArea.setWidgetResizable(True)
+        self.verticalLayout = QVBoxLayout(TaskView)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.SingleDirectionScrollArea = SingleDirectionScrollArea(TaskView)
+        self.SingleDirectionScrollArea.setObjectName(u"SingleDirectionScrollArea")
+        self.SingleDirectionScrollArea.setStyleSheet(u"QScrollArea{background: transparent; border: none}")
+        self.SingleDirectionScrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 780, 581))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 782, 582))
         self.scrollAreaWidgetContents.setStyleSheet(u"QWidget#scrollAreaWidgetContents { background: transparent }")
-        self.verticalLayout = QVBoxLayout(self.scrollAreaWidgetContents)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
@@ -44,18 +48,20 @@ class Ui_TaskView(object):
 
         self.horizontalLayout.addWidget(self.addTaskButton)
 
-        self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
 
-        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
+        self.SingleDirectionScrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verticalLayout.addWidget(self.SingleDirectionScrollArea)
+
 
         self.retranslateUi(TaskView)
 
         QMetaObject.connectSlotsByName(TaskView)
-
     # setupUi
 
     def retranslateUi(self, TaskView):
         TaskView.setWindowTitle(QCoreApplication.translate("TaskView", u"Form", None))
     # retranslateUi
+
