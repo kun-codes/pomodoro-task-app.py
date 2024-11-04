7dd25034a9b9d92f5c556979e4cac9f6d18943b8
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-11-04 20:23:12 +0530

    ui(settings view): change layout margins and spacings and scroll area widget

--- pomodoro-task-app/ui_py/ui_settings_view.py
+++ pomodoro-task-app/ui_py/ui_settings_view.py
@@ -3,18 +3,21 @@
 ################################################################################
 ## Form generated from reading UI file 'settings_view.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.2
+## Created by: Qt User Interface Compiler version 6.8.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
-                            Qt)
-from PySide6.QtWidgets import (QScrollArea, QSizePolicy, QVBoxLayout,
-                               QWidget)
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QVBoxLayout, QWidget)
 
-from qfluentwidgets import TitleLabel
-
+from qfluentwidgets import (ScrollArea, TitleLabel)
 
 class Ui_SettingsView(object):
     def setupUi(self, SettingsView):
@@ -23,16 +26,18 @@ class Ui_SettingsView(object):
         SettingsView.resize(648, 591)
         self.verticalLayout = QVBoxLayout(SettingsView)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.scrollArea = QScrollArea(SettingsView)
+        self.scrollArea = ScrollArea(SettingsView)
         self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setStyleSheet(u"QScrollArea{background: transparent; border: none}")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
         self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 630, 573))
-        self.scrollAreaWidgetContents.setStyleSheet(u"QWidget{background: transparent}")
+        self.scrollAreaWidgetContents.setStyleSheet(u"QWidget#scrollAreaWidgetContents{background: transparent}")
         self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setSpacing(28)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(36, 10, 36, 0)
         self.TitleLabel = TitleLabel(self.scrollAreaWidgetContents)
         self.TitleLabel.setObjectName(u"TitleLabel")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
@@ -40,22 +45,22 @@ class Ui_SettingsView(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.TitleLabel.sizePolicy().hasHeightForWidth())
         self.TitleLabel.setSizePolicy(sizePolicy)
-        self.TitleLabel.setAlignment(
-            Qt.AlignmentFlag.AlignLeading | Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self.TitleLabel.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
 
-        self.verticalLayout_2.addWidget(self.TitleLabel, 0, Qt.AlignmentFlag.AlignTop)
+        self.verticalLayout_2.addWidget(self.TitleLabel)
 
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.verticalLayout.addWidget(self.scrollArea)
 
+
         self.retranslateUi(SettingsView)
 
         QMetaObject.connectSlotsByName(SettingsView)
-
     # setupUi
 
     def retranslateUi(self, SettingsView):
         SettingsView.setWindowTitle(QCoreApplication.translate("SettingsView", u"Form", None))
         self.TitleLabel.setText(QCoreApplication.translate("SettingsView", u"Settings", None))
     # retranslateUi
+
