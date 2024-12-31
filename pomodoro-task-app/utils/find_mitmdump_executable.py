0231f51dd8d1f51396a3cd7608096b7e371238db
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-12-31 19:40:52 +0530

    feat(find_mitmdump_executable.py): modify it to find mitmdump binary in nuitka compiled form

--- pomodoro-task-app/utils/find_mitmdump_executable.py
+++ pomodoro-task-app/utils/find_mitmdump_executable.py
@@ -1,12 +1,31 @@
 import os
-import sys
+from pathlib import Path
 import shutil
 
-def get_mitmdump_path():
-    # Get the path to the Python interpreter in the virtual environment
-    venv_path = os.path.dirname(sys.executable)
+def is_nuitka():
+    # Check if the code is being run by Nuitka
+    return globals().get("__compiled__", False)
 
-    # Find the path to the mitmdump executable within the virtual environment
-    mitmdump_path = shutil.which("mitmdump", path=venv_path)
+def get_mitmdump_path():
+    print(f"globals: {globals()}")
+    if is_nuitka():
+        print("Running in Nuitka")
+        # Use the __nuitka_binary_dir from globals to find mitmdump
+        mitmdump_dir = Path(globals().get("__nuitka_binary_dir", "")).resolve()
+        print(f"mitmdump:  {mitmdump_dir}")
+        mitmdump_path = mitmdump_dir.joinpath("mitmdump").resolve()
+        print(f"mitmdump:  {mitmdump_path}")
+
+        if mitmdump_path.exists():
+            print(mitmdump_path)
+            return str(mitmdump_path)
+
+    print("Not Running in Nuitka")
+    # get the path to the project root
+    path = Path(__file__).parent.parent.parent
+    print(f"path: {path}")
+    # Find the path to the mitmdump executable
+    mitmdump_path = shutil.which("mitmdump", path=path)
+    print(f"mitmdump: {mitmdump_path}")
 
     return mitmdump_path
