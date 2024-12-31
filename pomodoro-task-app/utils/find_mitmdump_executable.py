8ed6e3ff991bce07b1c66409a5573b1af6bc942a
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2025-01-01 00:59:26 +0530

    feat(find_mitmdump_executable.py): add support to detect mitmdump in windows
    - also removed debug print statements

--- pomodoro-task-app/utils/find_mitmdump_executable.py
+++ pomodoro-task-app/utils/find_mitmdump_executable.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import shutil
+import platform
 
 def is_nuitka():
     # Check if the code is being run by Nuitka
@@ -12,20 +13,21 @@ def get_mitmdump_path():
         print("Running in Nuitka")
         # Use the __nuitka_binary_dir from globals to find mitmdump
         mitmdump_dir = Path(globals().get("__nuitka_binary_dir", "")).resolve()
-        print(f"mitmdump:  {mitmdump_dir}")
         mitmdump_path = mitmdump_dir.joinpath("mitmdump").resolve()
-        print(f"mitmdump:  {mitmdump_path}")
+
+        if platform.system() == "Windows":
+            mitmdump_path = mitmdump_path.with_suffix(".exe")
 
         if mitmdump_path.exists():
             print(mitmdump_path)
             return str(mitmdump_path)
 
-    print("Not Running in Nuitka")
     # get the path to the project root
     path = Path(__file__).parent.parent.parent
-    print(f"path: {path}")
     # Find the path to the mitmdump executable
     mitmdump_path = shutil.which("mitmdump", path=path)
-    print(f"mitmdump: {mitmdump_path}")
+
+    if platform.system() == "Windows" and mitmdump_path is None:
+        mitmdump_path = shutil.which("mitmdump.exe", path=path)
 
     return mitmdump_path
